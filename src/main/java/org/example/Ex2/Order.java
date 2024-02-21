de5357f280fe510724a3c98cf5d0ec5230918e89
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-21 17:34:47 +0100

    Update Order.java

--- src/main/java/org/example/Ex2/Order.java
+++ src/main/java/org/example/Ex2/Order.java
@@ -1,1 +1,25 @@
-package org.example.Ex2;public class Order {    private String nameOfDish;    public Order(String nameOfDish) {        this.nameOfDish = nameOfDish;    }    public String getNameOfDish() {        return nameOfDish;    }    public void setNameOfDish(String nameOfDish) {        this.nameOfDish = nameOfDish;    }    @Override    public String toString() {        return "Order {" +                "nameOfDish = '" + nameOfDish + '\'' +                '}';    }}
+package org.example.Ex2;
+
+public class Order {
+    private String nameOfDish;
+
+
+    public Order(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
+
+    public String getNameOfDish() {
+        return nameOfDish;
+    }
+
+    public void setNameOfDish(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
+
+    @Override
+    public String toString() {
+        return "Order {" +
+                "nameOfDish = '" + nameOfDish + '\'' +
+                '}';
+    }
+}
