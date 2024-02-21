cb64758d97039cbe879eaca43122222852a8c10e
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-21 17:35:06 +0100

    Update Waiter.java

--- src/main/java/org/example/Ex2/Waiter.java
+++ src/main/java/org/example/Ex2/Waiter.java
@@ -1,1 +1,27 @@
-package org.example.Ex2;public class Waiter {    private String name;    private Order order;    public Waiter(String name, Order order) {        this.name = name;        this.order = order;    }    public String getName() {        return name;    }    public void setName(String name) {        this.name = name;    }    public Order getOrder() {        return order;    }    public void setOrder(Order order) {        this.order = order;    }}
+package org.example.Ex2;
+
+public class Waiter {
+    private String name;
+    private Order order;
+
+    public Waiter(String name, Order order) {
+        this.name = name;
+        this.order = order;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
