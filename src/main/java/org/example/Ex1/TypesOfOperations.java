845b380a67c322be5101f13554ae7e1d24f92e6f
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-21 17:34:25 +0100

    Update TypesOfOperations.java

--- src/main/java/org/example/Ex1/TypesOfOperations.java
+++ src/main/java/org/example/Ex1/TypesOfOperations.java
@@ -1,1 +1,10 @@
-package org.example.Ex1;public enum TypesOfOperations {    CONSULTATION, // консультация    RECEIVING_FUNDS, // получение денежных средств    INVESTMENT, // вложение средств    OPENING_DEPOSIT, // открытие депозита    CLOSING_DEPOSIT, //  закрытие депозита    COMMUNAL_PAYMENTS // коммунальные платежи}
+package org.example.Ex1;
+
+public enum TypesOfOperations {
+    CONSULTATION, // консультация
+    RECEIVING_FUNDS, // получение денежных средств
+    INVESTMENT, // вложение средств
+    OPENING_DEPOSIT, // открытие депозита
+    CLOSING_DEPOSIT, //  закрытие депозита
+    COMMUNAL_PAYMENTS // коммунальные платежи
+}
